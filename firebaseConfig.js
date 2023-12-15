c24efae35e0f970a2baf759533665063dfc301b6
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2023-12-15 04:46:00 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyt9-vQ_s8IjgsGupmZwxhl084EAmTd_8",
-  authDomain: "avinash-mahesh-resume.firebaseapp.com",
-  databaseURL: "https://avinash-mahesh-resume-default-rtdb.firebaseio.com",
-  projectId: "avinash-mahesh-resume",
-  storageBucket: "avinash-mahesh-resume.appspot.com",
-  messagingSenderId: "939467001851",
-  appId: "1:939467001851:web:042ae7b98207046f22055e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
